16c228af8dc05377c7063d34b8ab65ead1e16c96
Tony Martinez <ajosephmartinez@gmail.com>
2022-01-16 14:16:44 +0000

    .

--- app/routes/jokes.tsx
+++ app/routes/jokes.tsx
@@ -1,5 +1,12 @@
 import {User} from '@prisma/client'
-import {Link, LinksFunction, LoaderFunction, Outlet, useLoaderData} from 'remix'
+import {
+  Link,
+  LinksFunction,
+  LoaderFunction,
+  Outlet,
+  useLoaderData,
+  Form,
+} from 'remix'
 import {db} from '~/utils/db.server'
 import {getUser} from '~/utils/session.server'
 import stylesUrl from '../styles/jokes.css'
@@ -49,11 +56,11 @@ export default function JokesRoute() {
           {data.user ? (
             <div className="user-info">
               <span>{`Hi ${data.user.username}`}</span>
-              <form action="/logout" method="post">
+              <Form action="/logout" method="post">
                 <button type="submit" className="button">
                   Logout
                 </button>
-              </form>
+              </Form>
             </div>
           ) : (
             <Link to="/login">Login</Link>
@@ -68,7 +75,9 @@ export default function JokesRoute() {
             <ul>
               {data.jokeListItems.map(joke => (
                 <li key={joke.id}>
-                  <Link to={joke.id}>{joke.name}</Link>
+                  <Link prefetch="intent" to={joke.id}>
+                    {joke.name}
+                  </Link>
                 </li>
               ))}
             </ul>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,5 @@
-import type {ActionFunction, LinksFunction} from 'remix'
+import type {ActionFunction, LinksFunction, MetaFunction} from 'remix'
+import {Form} from 'remix'
 import {useActionData, json, useSearchParams, Link} from 'remix'
 import {db} from '~/utils/db.server'
 import {createUserSession, login, register} from '~/utils/session.server'
@@ -6,6 +7,13 @@ import stylesUrl from '../styles/login.css'
 
 export const links: LinksFunction = () => {
   return [{rel: 'stylesheet', href: stylesUrl}]
+}
+
+export const meta: MetaFunction = () => {
+  return {
+    title: 'Remix Jokes | Login',
+    description: 'Login to submit your own jokes to Remix Jokes!',
+  }
 }
 
 function validateUsername(username: unknown) {
@@ -106,7 +114,7 @@ export default function Login() {
     <div className="container">
       <div className="content" data-light="">
         <h1>Login</h1>
-        <form
+        <Form
           method="post"
           aria-describedby={
             actionData?.formError ? 'form-error-message' : undefined
@@ -197,7 +205,7 @@ export default function Login() {
           <button type="submit" className="button">
             Submit
           </button>
-        </form>
+        </Form>
       </div>
       <div className="links">
         <ul>
